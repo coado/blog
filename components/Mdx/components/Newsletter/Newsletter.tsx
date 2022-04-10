7198c9513ec745f69beab538dbdc463f0deee402
coado <dawid.malecki21@gmail.com>
2022-04-10 21:29:10 +0200

    newsletter component

--- components/Mdx/components/Newsletter/Newsletter.tsx
+++ components/Mdx/components/Newsletter/Newsletter.tsx
@@ -3,7 +3,7 @@ import styles from './Newsletter.module.scss';
 export const Newsletter = () => {
     return (
         <div className={styles.newsletter}>
-            
+            <h2 className={styles.header} >Subscribe for more</h2>
             <div className={styles.wrapper}>
                 <input className={styles.input} type='email' placeholder='Email'/>
                 <button className={styles.subscribe}>Subscribe</button>
